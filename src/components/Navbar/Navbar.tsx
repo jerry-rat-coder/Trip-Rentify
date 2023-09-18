0ac5bd820256b2a74459c9a0eb18220f480f9757
DOC998 <3049988597@qq.com>
2023-09-18 16:31:05 +0800

    categories

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Categories from "./Categories";
 
 interface NavbarProps {
     currentUser: User | null
@@ -25,6 +26,7 @@ const Navbar: React.FC<NavbarProps> = ({
                     </div>
                 </Container>
             </div>
+            <Categories />
         </div>
      );
 }
